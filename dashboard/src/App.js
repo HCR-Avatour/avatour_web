83a26e27fe7fe16171556e61ca21542eb67f32d3
Chris Myers <cm1220@ic.ac.uk>
2024-03-11 15:45:08 +0000

    added button page

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -8,15 +8,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AIApp from "./pages/ai";
 import JoystickApp from "./pages/joystick";
 import MapApp from "./pages/map.js";
+import OnOffButton from "./pages/onOffButton.js";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={null} />
+        <Route
+          path="/"
+          element={<h1>If you are seeing this, it's working :D</h1>}
+        />
         <Route path="/joystick" element={<JoystickApp />} />
         <Route path="/ai" element={<AIApp />} />
         <Route path="/map" element={<MapApp />} />
+        <Route path="/button" element={<OnOffButton />} />
       </Routes>
     </Router>
   );
