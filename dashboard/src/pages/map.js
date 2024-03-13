2392cf121f938c44d2a615a19aee337bab111b88
Chris Myers <cm1220@ic.ac.uk>
2024-03-13 11:37:40 +0000

    map works

--- dashboard/src/pages/map.js
+++ dashboard/src/pages/map.js
@@ -5,19 +5,45 @@ import GoogleMap from "google-map-react";
 const MapApp = () => {
   const [position, setPosition] = useState({ latitude: null, longitude: null });
 
-  const Marker = ({ text }) => <div style={{ color: "red" }}>{text}</div>;
+  const Marker = ({ text }) => (
+    <div
+      style={{
+        borderRadius: 20,
+        backgroundColor: "#4181ec",
+        border: "solid",
+        color: "#4181ec",
+        width: "10px",
+        height: "10px",
+      }}
+    ></div>
+  );
 
   useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        setPosition({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      });
-    } else {
-      console.log("Geolocation is not available in your browser.");
-    }
+    const interval = setInterval(() => {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          function (position) {
+            setPosition({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            });
+          },
+          function () {
+            console.log("error");
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+          }
+        );
+      } else {
+        console.log("Geolocation is not available in your browser.");
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   const defaultProps = {
@@ -25,7 +51,7 @@ const MapApp = () => {
       lat: position.latitude,
       lng: position.longitude,
     },
-    zoom: 11,
+    zoom: 15,
   };
 
   return (
@@ -36,12 +62,13 @@ const MapApp = () => {
           bootstrapURLKeys={{ key: "AIzaSyDFIg5Kl3XDFflf0FZuhOJBIVARcWxQiOE" }}
           center={defaultProps.center}
           zoom={defaultProps.zoom}
+          options={{ gestureHandling: "none" }}
         >
-          {/* <Marker
+          <Marker
             lat={position.latitude}
             lng={position.longitude}
             text="Avatour"
-          /> */}
+          />
         </GoogleMap>
       </div>
     </div>
